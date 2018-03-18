a811b0d644c3ba29f6c0a18c4d029d40fc44fe2d
WARIFANSARI2006 <37344661+WARIFANSARI2006@users.noreply.github.com>
2018-03-17 19:33:42 -0700

    Add files via upload

---
+++ length4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main(){
+
+	char str1[3];
+	int length;
+	printf("enter the string\n");
+	gets(str1);
+	length=strlen(str1);
+	printf("length=%d",length);
+	getch();
+
+
+
+
+
+
+}
